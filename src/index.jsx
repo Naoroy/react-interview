8216bdcea5b57517b0c64f5e423a9a84eef84402
Naoroy <fegan.pierre@gmail.com>
2021-12-08 21:01:53 +0100

    Init

---
+++ src/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
+class App extends React.Component {
+  render() {
+    return(
+    <>
+      <p>Hey</p>
+    </>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('#app'))
+
